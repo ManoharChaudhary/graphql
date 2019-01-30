895b5da5719da0b0977c750403319a44d1c7e113
Manohar Magan Chaudhari <manohar.chaudhari@outlook.com>
2019-01-30 19:03:11 +0530

    #16 Adding Events

--- app.js
+++ app.js
@@ -2,25 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqaHttp = require("express-graphql");
 const mongoose = require("mongoose");
-const app = express();
 const graphQlSchema = require("./graphql/schema");
 const graphQlResolvers = require("./graphql/resolvers");
 const isAuth = require("./middleware/is-auth");
 
+const app = express();
 app.use(bodyParser.json());
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type",
-    "Authorization"
-  );
-  if(req.method == 'OPTIONS') {
-    return  res.sendStatus(200);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
   }
   next();
 });
+
 app.use(isAuth);
 
 app.use(
